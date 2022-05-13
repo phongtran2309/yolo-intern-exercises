f110e1372ecc7f434b3c80a0692c3caa03b08763
phongtrann <phongtran99932@gmail.com>
2022-05-13 17:07:19 +0700

    add exercise-11

---
+++ HARD/Exercise-11.js
@@ -0,0 +1,17 @@
+let number = [9];
+function countFactorial (number) {
+    let result = 1;
+    let fact = 1;
+    if(number > 1) {
+        for(i = 1; i <= number; i++){
+            fact *= i;
+        }
+        fact = fact + '';
+        for(let j = fact.length - 1; j >= 0; j--) {
+            if(fact[j] !== '0') {
+                return +fact[j];
+            }
+        }
+    }
+}
+console.log(countFactorial(number));
